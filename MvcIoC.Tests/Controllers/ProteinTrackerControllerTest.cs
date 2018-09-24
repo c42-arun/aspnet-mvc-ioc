79b14388e20d649f1c30b5be2dd739a35364a56a
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-24 12:18:41 +0100

    Unit tests changes

--- MvcIoC.Tests/Controllers/ProteinTrackerControllerTest.cs
+++ MvcIoC.Tests/Controllers/ProteinTrackerControllerTest.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MvcIoC.Controllers;
+using MvcIoC.Models;
 
 namespace MvcIoC.Tests.Controllers
 {
@@ -11,12 +12,37 @@ namespace MvcIoC.Tests.Controllers
         [TestMethod]
         public void WhenNothingHasHappenedTotalAndGoalAreZero()
         {
-            ProteinTrackerController controller = new ProteinTrackerController();
+            ProteinTrackerController controller = new ProteinTrackerController(new StubTrackingService());
 
             ViewResult result = controller.Index() as ViewResult;
 
             Assert.AreEqual(0, result.ViewBag.Total);
             Assert.AreEqual(0, result.ViewBag.Goal);
+        }
+
+        [TestMethod]
+        public void WhenTotalIsNonZero_AndAmountAdded_TotalIsIncreased()
+        {
+            var service = new StubTrackingService();
+            service.Total = 10;
+
+            ProteinTrackerController controller = new ProteinTrackerController(service);
+
+            ViewResult result = controller.AddProtein(15) as ViewResult;
+
+            Assert.AreEqual(25, result.ViewBag.Total);
+            Assert.AreEqual(0, result.ViewBag.Goal);
+        }
+
+        public class StubTrackingService : IProteinTrackingService
+        {
+            public int Total { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Goal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public void AddProtein(int amount)
+            {
+                Total += amount;
+            }
         }
     }
 }
